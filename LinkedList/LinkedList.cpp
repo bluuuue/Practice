2072d759354546e29cd226b85d23ef870424b516
bluuuue <yefanye@gmail.com>
2015-03-14 11:03:00 +0800

    add linkedlist file

---
+++ LinkedList/LinkedList.cpp
@@ -0,0 +1,20 @@
+// LinkedList.cpp : Defines the entry point for the console application.
+//
+
+#include "iostream"
+
+struct node
+{
+	char data;
+	node *next;
+};
+
+node *create();
+void showList(node *head);
+
+int main()
+{
+
+	return 0;
+}
+
