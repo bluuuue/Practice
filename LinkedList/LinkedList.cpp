8688b7c19a51f3268f05000c5cbc6f7635cbd142
bluuuue <yefanye@gmail.com>
2015-03-14 14:36:15 +0800

    modified

--- LinkedList/LinkedList.cpp
+++ LinkedList/LinkedList.cpp
@@ -19,3 +19,33 @@ int main()
 	return 0;
 }
 
+node *create()
+{
+	node *head = NULL;	//There is no node in the beginning, so head is NULL.
+	node *end = head;	//There is no element in the list, so end points to head.
+	node *pNew;	//the pointer used when inserting new node
+	char temp_data;	//store the temp data
+	cout << "Please input a string end with '#': " << endl;
+
+	do
+	{
+		cin >> temp_data;
+		if (temp_data != '#')
+		{
+			pNew = new node; 
+			pNew->data = temp_data;
+			pNew->next = NULL;
+			if (head == NULL)
+			{
+				head = pNew;
+			}
+			else
+			{
+				end->next = pNew;
+			}
+			end = pNew;
+		}
+	}while(temp_data != '#');
+
+	return head;
+}
