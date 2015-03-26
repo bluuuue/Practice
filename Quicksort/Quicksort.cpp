1526cf31ebaa1f624f1390b9dc0bccacafb9e876
bluuuue <yefanye@gmail.com>
2015-03-26 10:59:25 +0800

    20150325Quicksort

---
+++ Quicksort/Quicksort.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+struct Range
+{
+	explicit Range(int s = 0, int e = 0): start(s), end(e){}
+	int start, end;
+};
+
+int main()
+{
+	system("pause");
+	return 0;
+}
+
+void quicksort(int n, int arr[])
+{
+	if (n <= 0)
+	{
+		return;
+	}
+}
