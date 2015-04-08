af6f7845f96cacd477dcc8f88e938e1d34bb193d
bluuuue <yefanye@gmail.com>
2015-04-08 20:18:51 +0800

    04

--- JianzhiOffer/04_ReplaceBlank.cpp
+++ JianzhiOffer/04_ReplaceBlank.cpp
@@ -2,6 +2,7 @@
 #include<string.h>
 
 /*
+//the basic of character string
 int main()
 {
 	//the difference between char array and char pointer
@@ -27,11 +28,11 @@ int main()
 }
 */
 
+/*
+//put the result into a new char array
 int main()
 {
 	char input[] = "We are happy.";
-
-	//put the result into a new char array
 	int blank_count = 0;
 	for (int i = 0; i != strlen(input); ++i)
 	{
@@ -61,7 +62,61 @@ int main()
 	}
 	std::cout << std::endl;
 
+	return 0;
+}
+*/
 
+int ReplaceBlank(char str[], int length)
+{
+	if (str == NULL || length < 0)
+		return -1;
+
+	int old_length = strlen(str);
+	int number_of_blank = 0;
+	for (int i = 0; i != strlen(str); ++i)
+	{
+		if (str[i] == ' ')
+			++number_of_blank;
+	}
+
+	int new_length = strlen(str) + 2 * number_of_blank;
+	if (new_length > length)
+		return -1;
+
+	while (new_length >= 0 && new_length != old_length)
+	{
+		if (str[old_length] != ' ')
+		{
+			str[new_length--] = str[old_length--];
+		}
+		else
+		{
+			str[new_length--] = '%';
+			str[new_length--] = '0';
+			str[new_length--] = '2';
+			--old_length;
+		}
+	}
+
+	return 0;
+
+}
+
+int main()
+{
+	char str[20] = "We are happy.";
+	int length = 20;
+
+	if (ReplaceBlank(str, length) == -1)
+		std::cout << "wrong" << std::endl;
+
+	for(int i = 0; i != strlen(str); ++i)
+	{
+		std::cout << str[i];
+	}
+	std::cout << std::endl;
 
 	return 0;
 }
+
+//待考虑：如果将空格改为20%后超过原字符串大小，如何扩充原字符串
