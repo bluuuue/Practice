36b4f50709e838db8883b8581ea898a2607158a0
bluuuue <yefanye@gmail.com>
2015-05-13 10:27:10 +0800

    t

--- JianzhiOffer/16_ReverseList.cpp
+++ JianzhiOffer/16_ReverseList.cpp
@@ -2,6 +2,88 @@
 
 struct ListNode
 {
-	int			m_nKey;
+	int			m_nValue;
 	ListNode*	m_pNext;
 };
+
+void AddTOTail(ListNode **pHead, int value)
+{
+	ListNode *pNew = new ListNode();
+	pNew->m_nValue = value;
+	pNew->m_pNext = NULL;
+
+	if(*pHead == NULL)
+	{
+		*pHead = pNew;
+	}
+	else
+	{
+		ListNode *pNode = *pHead;
+
+		while(pNode->m_pNext != NULL)
+		{
+			pNode = pNode->m_pNext;
+		}
+
+		pNode->m_pNext = pNew;
+	}
+}
+ListNode* ReverseList(ListNode *pNode)
+{
+	if(pNode == NULL)
+	{
+		return NULL;
+	}
+
+	ListNode *pBehind = NULL;
+	ListNode *pIndex = pNode;
+	ListNode *pReversedHead= NULL;
+
+	while(pIndex != NULL)
+	{
+		ListNode *pAhead = pIndex->m_pNext;
+		if (pAhead == NULL)
+		{
+			pReversedHead = pIndex;
+		}
+		pIndex->m_pNext = pBehind;
+		pBehind = pIndex;
+		pIndex = pAhead;
+	}
+
+	return pReversedHead;
+}
+
+void PrintLinkedList(ListNode *list)
+{
+	if (list == NULL)
+	{
+		return;
+	}
+	
+	ListNode *index = list;
+	while (index != NULL)
+	{
+		std::cout << index->m_nValue << "\t";
+		index = index->m_pNext;
+	}
+	std::cout << std::endl;
+}
+
+int main()
+{
+	ListNode **pHead = new ListNode*();
+	*pHead = NULL;
+	for(int i = 0; i != 10; ++i)
+	{
+		AddTOTail(pHead, i);
+	}
+	PrintLinkedList(*pHead);
+
+	ListNode *pReverse = NULL;
+	pReverse = ReverseList(*pHead);
+	PrintLinkedList(pReverse);
+
+	return 0;
+}
+
