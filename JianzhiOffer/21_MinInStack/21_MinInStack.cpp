65944139ed4ae22db2b0d2032f43c004f5d8c3bf
bluuuue <yefanye@gmail.com>
2015-05-19 16:26:45 +0800

    t

--- JianzhiOffer/21_MinInStack/21_MinInStack.cpp
+++ JianzhiOffer/21_MinInStack/21_MinInStack.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include "..\21_MinInStack\21_MinInStack.h"
 
 int main()
 {
+	StackWithMin<int> stack;
+	stack.push(5);
+	stack.push(8);
+	stack.push(0);
+
+	while(stack.size() > 0)
+	{
+		std::cout << "Top: " << stack.top() << std::endl;
+		std::cout << "Min: " << stack.min() << std::endl;
+		stack.pop();
+	}
+
 
 	return 0;
 }
--- JianzhiOffer/21_MinInStack/21_MinInStack.h
+++ JianzhiOffer/21_MinInStack/21_MinInStack.h
@@ -25,4 +25,55 @@ private:
 	std::stack<T> m_min;
 };
 
-tem
+template <typename T> T& StackWithMin<T>::top(void)
+{
+	assert(m_data.size() > 0);
+
+	return m_data.top();
+}
+
+template <typename T> const T& StackWithMin<T>::top(void) const
+{
+	assert(m_data.size() > 0);
+
+	return m_data.top();
+}
+
+template <typename T> void StackWithMin<T>::push(const T& value)
+{
+	m_data.push(value);
+
+	if(m_min.size() == 0 || value < m_min.top())
+	{
+		m_min.push(value);
+	}
+	else
+	{
+		m_min.push(m_min.top());
+	}
+}
+
+template <typename T> void StackWithMin<T>::pop(void)
+{
+	assert(m_data.size() > 0 && m_min.size() > 0);
+
+	m_data.pop();
+	m_min.pop();
+}
+
+template <typename T> const T& StackWithMin<T>::min(void) const
+{
+	assert(m_data.size() > 0 && m_min.size() > 0);
+
+	return m_min.top();
+}
+
+template <typename T> bool StackWithMin<T>::empty(void) const
+{
+	return m_data.empty();
+}
+
+template <typename T> size_t StackWithMin<T>::size(void) const
+{
+	return m_data.size();
+}
