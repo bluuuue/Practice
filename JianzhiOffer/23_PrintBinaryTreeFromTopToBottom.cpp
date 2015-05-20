45ba2419f7a3d05e19a9929a8f6ebea5bbee8368
bluuuue <yefanye@gmail.com>
2015-05-20 11:32:31 +0800

    t

---
+++ JianzhiOffer/23_PrintBinaryTreeFromTopToBottom.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+struct BinaryTreeNode
+{
+	int				m_nValue;
+	BinaryTreeNode*	m_pLeft;
+	BinaryTreeNode*	m_pRight;
+}*pBinaryTree;
+
+pBinaryTree CreateBinaryTree(pBinaryTree &pTreeRoot)
+{
+}
+
+int main()
+{
+
+}
