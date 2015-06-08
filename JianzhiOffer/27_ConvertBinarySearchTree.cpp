a05e3afddd65c54baeeb3371f84e03280e6d9c24
bluuuue <yefanye@gmail.com>
2015-06-08 08:14:51 +0800

    t

--- JianzhiOffer/27_ConvertBinarySearchTree.cpp
+++ JianzhiOffer/27_ConvertBinarySearchTree.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 #include "BinaryTree.h"
 
+void ConvertNode(BinaryTreeNode* pNode, BinaryTreeNode** pLastNodeInList)
+{
+    if(pNode == NULL)
+    {
+        return;
+    }
+
+    BinaryTreeNode *pCurrent = pNode;
+
+    if(pCurrent->m_pLeft != NULL)
+    {
+        ConvertNode(pCurrent->m_pLeft, pLastNodeInList);
+    }
+
+    pCurrent->m_pLeft = 
+
+
+BinaryTreeNode* Convert(BinaryTreeNode* pRootOfTree)
+{
+    BinaryTreeNode *pLastNodeInList = NULL;
+    ConvertNode(pRootOfTree, &pLastNodeInList);
+
+}
+
 int main()
 {
 	BinaryTreeNode *pRoot = NULL;
