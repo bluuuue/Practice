1f969e38c9e7714470d01b8e583bea5f4d1e4db9
bluuuue <yefanye@gmail.com>
2015-06-08 11:16:30 +0800

    t

--- JianzhiOffer/27_ConvertBinarySearchTree.cpp
+++ JianzhiOffer/27_ConvertBinarySearchTree.cpp
@@ -15,13 +15,31 @@ void ConvertNode(BinaryTreeNode* pNode, BinaryTreeNode** pLastNodeInList)
         ConvertNode(pCurrent->m_pLeft, pLastNodeInList);
     }
 
-    pCurrent->m_pLeft = 
+	pCurrent->m_pLeft = *pLastNodeInList;
+	if (*pLastNodeInList != NULL)
+	{
+		(*pLastNodeInList)->m_pRight = pCurrent;
+	}
 
+	*pLastNodeInList = pCurrent;
+
+	if (pCurrent->m_pRight != NULL)
+	{
+		ConvertNode(pCurrent->m_pRight, pLastNodeInList);
+	}
+}
 
 BinaryTreeNode* Convert(BinaryTreeNode* pRootOfTree)
 {
     BinaryTreeNode *pLastNodeInList = NULL;
     ConvertNode(pRootOfTree, &pLastNodeInList);
+
+	BinaryTreeNode *pHeadOfList = pLastNodeInList;
+	while (pHeadOfList != NULL && pHeadOfList->m_pLeft != NULL)
+	{
+		pHeadOfList = pHeadOfList->m_pLeft;
+	}
+	return pHeadOfList;
 
 }
 
@@ -32,5 +50,9 @@ int main()
 	CreateBinaryTree(pRoot);
 	PrintBinaryTree(pRoot);
 
+	BinaryTreeNode *pResult = NULL;
+	pResult = Convert(pRoot);
+
 	return 0;
 }
+
