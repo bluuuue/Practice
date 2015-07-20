6b6663fa7efcf1b6b46ec08859e02a794ed0ccb5
bluuuue <yefanye@gmail.com>
2015-07-20 21:35:01 +0800

    t

--- JianzhiOffer/33_SortArrayForMinNumber.cpp
+++ JianzhiOffer/33_SortArrayForMinNumber.cpp
@@ -1,11 +1,30 @@
 #include <iostream>
+#include <iostream>
 
-int MinNUmberOfArray(int data[], int nLength)
+const int g_nMaxNumberLength = 10;
+
+char *g_StrCombine1 = new char[g_nMaxNumberLength * 2 + 1];
+char *g_StrCombine2 = new char[g_nMaxNumberLength * 2 + 1];
+
+int Compare(const void *strNumber1, const void *strNumber2)
+{
+	strcpy(g_StrCombine1, *(const char **)strNumber1);
+	strcat(g_StrCombine1, *(const char **)strNumber2);
+
+	strcpy(g_StrCombine2, *(const char **)strNumber2);
+	strcat(g_StrCombine2, *(const char **)strNumber1);
+
+	return strcmp(g_StrCombine1, g_StrCombine2);
+}
+
+int PrintMinNUmberOfArray(int *numbers, int nLength)
 {
 }
 
 int main()
 {
-
+	int data[3] = {3, 32, 321};
+	PrintMinNumberOfArray(data);
     return 0;
+}
 }
