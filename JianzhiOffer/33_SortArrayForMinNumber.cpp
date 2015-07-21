9f228806c3898e417a313de80ddb7729672da08b
bluuuue <yefanye@gmail.com>
2015-07-21 19:53:43 +0800

    t

--- JianzhiOffer/33_SortArrayForMinNumber.cpp
+++ JianzhiOffer/33_SortArrayForMinNumber.cpp
@@ -1,5 +1,6 @@
-#include <iostream>
-#include <iostream>
+#include "stdio.h"
+#include "string.h"
+#include "stdlib.h"
 
 const int g_nMaxNumberLength = 10;
 
@@ -17,14 +18,40 @@ int Compare(const void *strNumber1, const void *strNumber2)
 	return strcmp(g_StrCombine1, g_StrCombine2);
 }
 
-int PrintMinNUmberOfArray(int *numbers, int nLength)
+int PrintMinNumberOfArray(int *numbers, int nLength)
 {
+	if (numbers == NULL || nLength <= 0)
+	{
+		return -1;
+	}
+
+	char **strNumbers = (char**)(new int[nLength]);
+	for (int i = 0; i < nLength; ++i)
+	{
+		strNumbers[i] = new char[g_nMaxNumberLength + 1];
+		sprintf(strNumbers[i], "%d", numbers[i]);
+	}
+
+	qsort(strNumbers, nLength, sizeof(char*), Compare);
+
+	for (int i = 0; i < nLength; ++i)
+	{
+		printf("%s", strNumbers[i]);
+	}
+	printf("\n");
+
+	for (int i = 0; i < nLength; ++i)
+	{
+		delete[] strNumbers[i];
+	}
+	delete[] strNumbers;
+
+	return 0;
 }
 
 int main()
 {
 	int data[3] = {3, 32, 321};
-	PrintMinNumberOfArray(data);
+	PrintMinNumberOfArray(data, 3);
     return 0;
-}
 }
