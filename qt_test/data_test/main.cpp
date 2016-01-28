b014d2a9eb3a1785a0ba31661e796d5382fcfd4d
bluuuue <yefanye@gmail.com>
2016-01-28 20:26:04 +0800

    t

--- qt_test/data_test/main.cpp
+++ qt_test/data_test/main.cpp
@@ -2,6 +2,7 @@
 #include <QApplication>
 #include <iostream>
 #include <QtSql>
+#include <QFileDialog>
 
 int main(int argc, char *argv[])
 {
@@ -9,21 +10,27 @@ int main(int argc, char *argv[])
     MainWindow w;
     //std::cout << "ads" << std::endl;
     QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE"); //添加数据库驱动
-    db.setDatabaseName("test.db"); //数据库连接命名
+    //QString filename = QFileDialog::getOpenFileName(this, tr("data.db"), true);
+    db.setDatabaseName("data.db"); //数据库连接命名
     if(!db.open()) //打开数据库
     {
         return false;
     }
 
     QSqlQuery query; //以下执行相关QSL语句
-    query.exec("create table student(id int primary key,name varchar)");
+    /*
+    query.exec("create table students(id int primary key,name varchar)");
+    query.exec();
     //新建student表，id设置为主键，还有一个name项
-    query.exec("insert into student values(1,'xiaogang')");
-    query.exec("insert into student values(2,'xiaoming')");
-    query.exec("insert into student values(3,'xiaohong')");
+    query.exec("insert into students values(1,'xiaogang')");
+    query.exec();
+    query.exec("insert into students values(4,'xiaoling')");
+    query.exec();
+    query.exec("insert into students values(3,'xiaohong')");
+    query.exec();
     //向表中插入3条记录
-
-    query.exec("select id,name from student where id >= 2");
+    */
+    query.exec("select id,name from students where id >= 2");
     //查找表中id >=2 的记录的id项和name项的值
     while(query.next())       //query.next()指向查找到的第一条记录，然后每次后移一条记录
     {
@@ -31,6 +38,7 @@ int main(int argc, char *argv[])
         QString ele1 =query.value(1).toString();
         qDebug() << ele0 <<ele1 ;      //输出两个值
     }
+
 
     w.show();
 
